08f2697f7d587c4db7f6f9fd0b3ee8dedb8cd3ec
Mr. Velox <haider13ahmed2@gmail.com>
2026-03-28 20:06:59 +0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -6,6 +6,36 @@
 #include "PRX.h"
 #include "INI.h"
 
+int checkFileExists(const char* filepath) {
+  int fd = open(filepath, O_RDONLY, 0);
+  if (fd >= 0) {
+    close(fd);
+    return 1;
+  }
+  return 0;
+}
+
+int checkStringInFile(const char* filepath, const char* searchStr) {
+  int fd = open(filepath, O_RDONLY, 0);
+  if (fd < 0) return 0;
+  char buf[2048];
+  int bytesRead = read(fd, buf, sizeof(buf) - 1);
+  close(fd);
+  if (bytesRead <= 0) return 0;
+  buf[bytesRead] = '\0';
+  
+  for (int i = 0; buf[i] != '\0'; i++) {
+    int j = 0;
+    while (searchStr[j] != '\0' && buf[i + j] == searchStr[j]) {
+      j++;
+    }
+    if (searchStr[j] == '\0') {
+      return 1;
+    }
+  }
+  return 0;
+}
+
 void writePRX(){
   printf_notification("Start adding fix, please wait...");
   size_t len = 0;
@@ -30,7 +60,7 @@ void writeINI(){
   sceKernelSleep(5);
 	if (len != 0){
     mkdir("/data/GoldHEN", 0777);
-    int fid = open("/data/GoldHEN/plugins.ini", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    int fid = open("/data/GoldHEN/plugins.ini", O_WRONLY | O_CREAT | O_APPEND, 0777);
     write(fid, fbuf, len);
     close(fid);
     sceKernelSleep(5);
@@ -59,11 +89,19 @@ int _main(struct thread *td) {
   get_firmware_string(fw_version);
   printf_notification("PSFree-Fix\nPS4 Firmware %s", fw_version);
   
-  
-  writeINI();
+  int prx_exists = checkFileExists("/data/GoldHEN/plugins/aio_fix_505.prx");
+  int ini_has_path = checkStringInFile("/data/GoldHEN/plugins.ini", "aio_fix_505.prx");
 
-  writePRX();
-  
+  if (prx_exists && ini_has_path) {
+    printf_notification("Fix is already installed");
+  } else {
+    if (!ini_has_path) {
+      writeINI();
+    }
+    if (!prx_exists) {
+      writePRX();
+    }
+  }
 
 #ifdef DEBUG_SOCKET
   printf_debug("Closing socket...\n");
